741e0967c0ebcd9973064b227404ffcb85b18021
tailgear <a@kovyrev.ru>
2023-08-13 22:52:09 +0300

    create

---
+++ post_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+USER="admin"
+PASS=$(cat /dev/urandom | strings | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+CFG_FLDR="/usr/local/etc/syncthing"
+
+# Enable the service
+sysrc -f /etc/rc.conf syncthing_enable="YES"
+
+syncthing generate --gui-user=$USER --gui-password=$PASS --no-default-folder --config=$CFG_FLDR
+sed -i '' 's|<address>127.0.0.1:8384</address>|<address>0.0.0.0:8384</address>|g' $CFG_FLDR/config.xml
+chown syncthing:syncthing $CFG_FLDR/config.xml
+
+mkdir /data
+chown syncthing:syncthing /data
+
+echo "Syncthing Configuration:" > /root/PLUGIN_INFO
+echo "• GUI User: ${USER}" >> /root/PLUGIN_INFO
+echo "• GUI Password: ${PASS}" >> /root/PLUGIN_INFO
+
+# Start the service
+service syncthing start 2>/dev/null
