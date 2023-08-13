40d8bb39dc1c919c1bd6bf385230fb24c969d217
tailgear <a@kovyrev.ru>
2023-08-13 23:41:50 +0300

    fix2

--- post_install.sh
+++ post_install.sh
@@ -9,7 +9,7 @@ sysrc -f /etc/rc.conf syncthing_enable="YES"
 
 syncthing generate --gui-user=$USER --gui-password=$PASS --no-default-folder --home=$HOME_FLDR
 sed -i '' 's|<address>127.0.0.1:8384</address>|<address>0.0.0.0:8384</address>|g' $HOME_FLDR/config.xml
-chown syncthing:syncthing $HOME_FLDR/*
+chown -R syncthing:syncthing $HOME_FLDR
 
 mkdir /data
 chown syncthing:syncthing /data
